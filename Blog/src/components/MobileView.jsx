f59408a7f92b9f8060b648b541aee206dd1de51a
Neetu Rai <raineetu0070@gmail.com>
2025-07-22 19:22:31 +0545

    feat: fixed mobile view

--- Blog/src/components/MobileView.jsx
+++ Blog/src/components/MobileView.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const MobileView = () => {
-  return <div></div>;
+const MobileView = ({ isopen, setOpen }) => {
+  return (
+    <div
+      className={`flex md:hidden flex-col gap-6 py-20 px-10 fixed top-0 bottom-0 right-0 w-64 shadow-lg z-50 h-screen bg-rose-50 transition-transform duration-300 transform ${
+        isopen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      {/* Close Button */}
+      <div className="absolute top-5 right-5">
+        <button
+          className="text-2xl font-bold text-gray-700"
+          onClick={() => setOpen(false)}
+        >
+          ✕
+        </button>
+      </div>
+
+      {/* Menu Items */}
+      <ul className="flex flex-col gap-4 mt-10 text-lg font-semibold">
+        <li onClick={() => setOpen(false)}>
+          <a href="#top">Home</a>
+        </li>
+        <li onClick={() => setOpen(false)}>
+          <a href="#about">About Us</a>
+        </li>
+        <li onClick={() => setOpen(false)}>
+          <a href="#blog">Blog</a>
+        </li>
+        <li onClick={() => setOpen(false)}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default MobileView;
--- Blog/src/components/Navbar.jsx
+++ Blog/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MobileView from "./MobileView";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   const [isopen, setOpen] = useState(false);
@@ -13,7 +14,7 @@ const Navbar = () => {
         </div>
 
         {/* nav items */}
-        <div>
+        <div className="hidden md:flex">
           <nav>
             <ul className="flex items-center justify-between space-x-8  text-xl cursor-pointer">
               <li>Home</li>
@@ -25,13 +26,18 @@ const Navbar = () => {
         </div>
 
         {/* login */}
-        <div>
-          <button className=" rounded-full px-6 py-2 font-semibold cursor-pointer">
-            login
-          </button>
-          <button className="bg-black text-white rounded-full px-5 py-2 font-semibold cursor-pointer">
-            Sign up
-          </button>
+        <div className="flex gap-4">
+          <div>
+            <button className=" rounded-full px-6 py-2 font-semibold cursor-pointer">
+              login
+            </button>
+            <button className="bg-black text-white rounded-full px-5 py-2 font-semibold cursor-pointer">
+              Sign up
+            </button>
+          </div>
+          <div className="md:hidden mt-3" onClick={() => setOpen(!isopen)}>
+            <Menu className="cursor-pointer" />
+          </div>
         </div>
       </div>
       {isopen && <MobileView isopen={isopen} setOpen={setOpen} />}
