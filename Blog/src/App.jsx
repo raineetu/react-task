ff41d4c3f5b0492eb4cb1909a3251ba756648f00
Neetu Rai <raineetu0070@gmail.com>
2025-07-22 18:59:34 +0545

    feat:added new font

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 };
 
---
+++ Blog/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import MobileView from "./MobileView";
+
+const Navbar = () => {
+  const [isopen, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="flex justify-around items-center shadow-md  ">
+        {/* logo */}
+        <div>
+          <img src="./blog.png" alt="logo" className="w-34 h-34" />
+        </div>
+
+        {/* nav items */}
+        <div>
+          <nav>
+            <ul className="flex items-center justify-between space-x-8  text-xl cursor-pointer">
+              <li>Home</li>
+              <li>About us</li>
+              <li>Blog</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* login */}
+        <div>
+          <button className=" rounded-full px-6 py-2 font-semibold cursor-pointer">
+            login
+          </button>
+          <button className="bg-black text-white rounded-full px-5 py-2 font-semibold cursor-pointer">
+            Sign up
+          </button>
+        </div>
+      </div>
+      {isopen && <MobileView isopen={isopen} setOpen={setOpen} />}
+    </>
+  );
+};
+
+export default Navbar;
